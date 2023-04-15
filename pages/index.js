497a6e3fa780adf37e22e50491760b36ac752d12
Marcello Antunes <marcello@Marcellos-MacBook-Air.local>
2023-04-15 13:21:30 -0300

    feat: add auth0 login api route

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <div>
       <p>Home</p>
+      <div>
+        <Link href="/api/auth/login">Login</Link>
+      </div>
     </div>
   );
 };
