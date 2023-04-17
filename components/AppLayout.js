937490246940e73c56e01f06231c675d534e7b1d
Marcello Antunes <marcello@Marcellos-MacBook-Air.local>
2023-04-17 19:10:00 -0300

    feat: create app layout component

---
+++ components/AppLayout.js
@@ -0,0 +1,9 @@
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      this is the app layout <div>{children}</div>
+    </div>
+  );
+};
+
+export default AppLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 const Home = () => {
+  const { user } = useUser();
+
   return (
     <div>
-      <p>Home</p>
       <div>
-        <Link href="/api/auth/login">Login</Link>
+        {!!user ? (
+          <div>
+            <Image width={50} height={50} alt={user.name} src={user.picture} />
+            <div>{user.email}</div>
+            <Link href="/api/auth/logout">Logout</Link>
+          </div>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
       </div>
     </div>
   );
