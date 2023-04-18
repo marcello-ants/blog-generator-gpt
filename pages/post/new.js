b52b61f7c602a22cf4b259a8a2c8a011a99ac10c
Marcello Antunes <marcello@Marcellos-MacBook-Air.local>
2023-04-18 20:45:29 -0300

    feat: create generate post api route

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import AppLayout from "../../components/AppLayout";
 
 const NewPost = (props) => {
+  const [postContent, setPostContent] = useState("");
+
+  const handleClick = async () => {
+    const response = await fetch(`/api/generatePost`, {
+      method: "POST",
+    });
+
+    const json = await response.json();
+    setPostContent(json.post.postContent);
+  };
+
   return (
     <div>
       <h1>new post</h1>
+      <button onClick={handleClick}>generate</button>
+      <div
+        className="max-width-screen-sm p-10"
+        dangerouslySetInnerHTML={{ __html: postContent }}
+      />
     </div>
   );
 };
