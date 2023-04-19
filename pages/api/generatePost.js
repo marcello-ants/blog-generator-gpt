b8930f30b1190efc751e9280056d80642a0b78e8
Marcello Antunes <marcello@Marcellos-MacBook-Air.local>
2023-04-19 19:54:05 -0300

    feat: add insert post to mongodb

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,6 +1,22 @@
+import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
 import { Configuration, OpenAIApi } from "openai";
+import clientPromise from "../../lib/mongodb";
 
 const handler = async (req, res) => {
+  const { user } = await getSession(req, res);
+
+  const client = await clientPromise;
+  const db = client.db("blogify-ai");
+
+  const userProfile = await db.collection("users").findOne({
+    auth0Id: user.sub,
+  });
+
+  if (!userProfile?.availableTokens) {
+    res.status(403);
+    return;
+  }
+
   const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
@@ -80,6 +96,27 @@ const handler = async (req, res) => {
   const metaDescription =
     metaDescriptionResult.data.choices[0]?.message.content;
 
+  await db.collection("users").updateOne(
+    {
+      auth0Id: user.sub,
+    },
+    {
+      $inc: {
+        availableTokens: -1,
+      },
+    }
+  );
+
+  const post = await db.collection("posts").insertOne({
+    postContent: postContent || "",
+    title: title || "",
+    metaDescription: metaDescription || "",
+    topic,
+    keywords,
+    userId: userProfile._id,
+    created: new Date(),
+  });
+
   res.status(200).json({
     post: {
       title,
@@ -89,4 +126,4 @@ const handler = async (req, res) => {
   });
 };
 
-export default handler;
+export default withApiAuthRequired(handler);
